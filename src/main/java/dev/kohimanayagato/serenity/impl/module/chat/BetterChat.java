96a9b6b27c785901d18fb7de70885f086238f724
L-Nadav <45271277+L-Nadav@users.noreply.github.com>
2020-10-23 16:35:24 +0100

    Add files via upload

---
+++ src/main/java/dev/kohimanayagato/serenity/impl/module/chat/BetterChat.java
@@ -0,0 +1,50 @@
+package dev.kohimanayagato.serenity.impl.module.chat;
+
+import dev.kohimanayagato.serenity.api.module.Category;
+import dev.kohimanayagato.serenity.api.module.Module;
+import net.minecraftforge.client.event.ClientChatEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import dev.kohimanayagato.serenity.api.util.LoggerUtil;
+import dev.kohimanayagato.serenity.api.setting.Setting;
+import dev.kohimanayagato.serenity.api.setting.SettingType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
+public class BetterChat extends Module
+{
+	private final Setting Mode = new Setting("Mode", this, Arrays.asList(
+		"Append",
+		"Replace"
+		));
+		
+	public BetterChat(String name, String description, Category category)
+	{
+		super(name, description, category);
+		addSetting(Mode);
+		
+	}
+
+	@SubscribeEvent
+	public void onChat(ClientChatEvent event)
+	{
+		if ( Mode.getEnumValue().equalsIgnoreCase("Append") ){
+			if ( event.getMessage().contains("shrug")) {
+				event.setMessage(event.getMessage().replace("shrug", "") + " \u00AF\\_(\u30C4)_/\u00AF");
+			}
+			if ( event.getMessage().contains("lenny")) {
+				event.setMessage(event.getMessage().replace("lenny", "") + " ( ͡° ͜ʖ ͡°)");
+			}
+		}
+		if ( Mode.getEnumValue().equalsIgnoreCase("Replace") ){
+			if ( event.getMessage().contains("shrug") ) {
+				event.setMessage(event.getMessage().replace("shrug", "\u00AF\\_(\u30C4)_/\u00AF"));
+			}
+			if ( event.getMessage().contains("lenny") ) {
+				event.setMessage(event.getMessage().replace("lenny", " ( ͡° ͜ʖ ͡°) "));
+			}
+		}
+	}
+}
