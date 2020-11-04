09225ae85e80a6b310b83c64a39d020971aa0a56
Denomi <sophiekohima@gmail.com>
2020-11-04 17:02:38 +0000

    Add Comands Boolean to ChatSuffix

--- src/main/java/dev/kohimanayagato/serenity/impl/module/chat/ChatSuffix.java
+++ src/main/java/dev/kohimanayagato/serenity/impl/module/chat/ChatSuffix.java
@@ -4,22 +4,30 @@ import dev.kohimanayagato.serenity.api.module.Category;
 import dev.kohimanayagato.serenity.api.module.Module;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import dev.kohimanayagato.serenity.api.setting.Setting;
+import dev.kohimanayagato.serenity.api.setting.SettingType;
 
 import java.util.Arrays;
 
 public class ChatSuffix extends Module
 {
+
+	private final Setting useCommands = new Setting("Commands", this, true);
+
 	public ChatSuffix(String name, String description, Category category)
 	{
 		super(name, description, category);
+		addSetting(useCommands);
 	}
 
 	@SubscribeEvent
 	public void onChat(ClientChatEvent event)
 	{
-		for (String s : Arrays.asList("/", ".", "-", ",", ":", ";", "'", "\"", "+", "\\", "$", "*"))
-		{
-			if (event.getMessage().startsWith(s)) return;
+		if ( !useCommands.getBooleanValue() ){
+			for (String s : Arrays.asList("/", ".", "-", ",", ":", ";", "'", "\"", "+", "\\", "$", "*"))
+			{
+				if (event.getMessage().startsWith(s)) return;
+			}
 		}
 
 		event.setMessage(event.getMessage() + " \uff5c Serenity");
