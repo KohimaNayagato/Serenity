6f63995ad20fdd299ddc52446b52ad210071f722
Denomi <sophiekohima@gmail.com>
2020-11-28 15:53:12 +0000

    [FIX] ChatSuffix
    No longer displays Suffix when fetching previously sent messages

--- src/main/java/dev/kohimanayagato/serenity/impl/module/chat/ChatSuffix.java
+++ src/main/java/dev/kohimanayagato/serenity/impl/module/chat/ChatSuffix.java
@@ -1,35 +1,49 @@
 package dev.kohimanayagato.serenity.impl.module.chat;
 
+
+import dev.kohimanayagato.serenity.Client;
 import dev.kohimanayagato.serenity.api.module.Category;
 import dev.kohimanayagato.serenity.api.module.Module;
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import dev.kohimanayagato.serenity.api.setting.Setting;
-import dev.kohimanayagato.serenity.api.setting.SettingType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ChatSuffix extends Module
 {
 
-	private final Setting useCommands = new Setting("Commands", this, true);
+
+	private final ArrayList<String> prefixes = new ArrayList<>(Arrays.asList("/", ".", "-", ",", ":", ";", "'", "\"", "+", "\\"));
 
 	public ChatSuffix(String name, String description, Category category)
 	{
 		super(name, description, category);
-		addSetting(useCommands);
+
 	}
 
+
 	@SubscribeEvent
-	public void onChat(ClientChatEvent event)
+	public void onMessage(ClientChatEvent event)
 	{
-		if ( !useCommands.getBooleanValue() ){
-			for (String s : Arrays.asList("/", ".", "-", ",", ":", ";", "'", "\"", "+", "\\", "$", "*"))
-			{
-				if (event.getMessage().startsWith(s)) return;
-			}
+		if (mc.player == null || mc.world == null || event.getMessage().startsWith(Client.commandManager.getPrefix()))
+		{
+			return;
 		}
 
-		event.setMessage(event.getMessage() + " \uff5c Serenity");
+		for (String prefix : prefixes)
+		{
+			if (event.getMessage().startsWith(prefix)) return;
+		}
+
+		String msg;
+
+
+		{
+			msg = String.format("%s \uFF5C Serenity", event.getMessage());
+		}
+
+		event.setMessage(msg);
+
 	}
 }
